85ab450a7f4f4f238852c5f4f19b8bd70543c441
Eliton Camargo <camargoliveira@gmail.com>
2024-09-04 21:46:34 -0300

    Testando vercel

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
-const urlApi = "http://100.26.59.163:8081";
+const urlApi = "https://sgqui.vercel.app";
+// const urlApi = "http://100.26.59.163:8081";
 //const urlApi = "http://localhost:8081";
 
 const opt = {
