6d20d79208920fe211b8cc41eec0d4511d9f8c17
ELiton Camargo <camargoliveira@gmail.com>
2025-06-10 08:55:14 -0300

    Atualizando module types

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Emular __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-
-// Middleware para redirecionar HTTPS para HTTP (opcional)
+// Middleware opcional para redirecionar HTTPS para HTTP
 app.use((req, res, next) => {
     if (req.secure) {
         return res.redirect(301, `http://${req.headers.host}${req.url}`);
@@ -15,13 +22,16 @@ app.use((req, res, next) => {
 // Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Roteamento SPA: redireciona todas as requisições desconhecidas para index.html
+// Roteamento SPA
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Iniciar o servidor
-const PORT = 3030; // Altere a porta se necessário
+const PORT = process.env.PORT || 8080; // Use a porta definida no .env ou 8080 por padrão
+if (!process.env.PORT) {
+    console.warn('Aviso: A variável de ambiente PORT não está definida. Usando a porta 8080 por padrão.');
+}
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
